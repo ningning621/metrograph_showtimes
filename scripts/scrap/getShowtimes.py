8f47820a57b7513cb8d6bbf227597fca72d3af78
lesley <ningning621@gmail.com>
2025-09-13 00:40:38 -0400

    parse rest of film

--- scripts/scrap/getShowtimes.py
+++ scripts/scrap/getShowtimes.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import csv
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -28,9 +29,18 @@ def get_metrograph_films(isLocal: bool):
 
     # parse raw information into list of films
     for film in films:
+        # print(film)
         title = film.find("h3", class_="movie_title").get_text(strip=True)
 
         raw_director = film.find("h5", string=lambda t: t and "Director" in t)
+
+        pattern = re.compile(r'^\s*(\d{3,4})\s*/\s*(\d+)\s*min(?:\s*/.*)?$', re.IGNORECASE)
+        raw_year_duration = film.find(
+            "h5",
+            string=lambda t: isinstance(t, str) and pattern.match(t.strip())
+        )
+        print(raw_year_duration)
+        year = int(raw_year_duration.get_text(strip=True).split("/")[0].strip()) if raw_year_duration else 0
 
         if raw_director:
             director_text = raw_director.get_text(strip=True).replace("Director:", "").strip()
@@ -40,37 +50,44 @@ def get_metrograph_films(isLocal: bool):
         synopsis = film.find("p", class_="synopsis").get_text(strip=True)
         imageUrl = film.find("img").attrs["src"]
 
-        parsed_films.append({"title": title, "imageUrl": imageUrl, "directors": directors, "synopsis": synopsis })
+        parsed_films.append({"title": title, "imageUrl": imageUrl, "directors": directors, "synopsis": synopsis, "year": year })
 
     # add metrograph html to file for local storage 
     with open("./scripts/scrap/raw_films.json", "w", encoding="utf-8") as f:
         json.dump(parsed_films, f, ensure_ascii=False, indent=2)
 
 def parse_letterboxd():
-    with open("./scripts/scrap/raw_films.json", "r", encoding="utf-8") as f:
+    with open("./scripts/scrap/raw_films2.json", "r", encoding="utf-8") as f:
         parsed_films = json.load(f)
 
     # set up selenium
     options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--blink-settings=imagesEnabled=false")
+    # options.add_argument("--headless=new")
+    # options.add_argument("--blink-settings=imagesEnabled=false")
     driver = webdriver.Chrome(options=options)
 
     skipped_films = []
     done_films = []
 
+    skip_phrases = [
+        "ace presents",
+        "afternoon cartoon",
+        "best of nyc",
+        "private event today",
+        "preceded by",
+        "presents:",
+        "short film program",
+    ]
+
     for film in parsed_films:
+        print(film)
         film_title = film["title"]
 
-        if ("ace presents" in film_title.lower() 
-            or 'private event' in film_title.lower() 
-            or 'afternoon cartoons' in film_title.lower()
-            or 'preceded by' in film_title.lower()
-            or 'multiple dirs' in film['directors']):
+        if (any(p in film_title.lower() for p in skip_phrases) or 'multiple dirs' in film['directors']):
             skipped_films.append(film)
         else: 
             try:
-                driver.get("https://letterboxd.com/search/" + quote_plus(film_title))
+                driver.get("https://letterboxd.com/search/" + quote_plus(f"{film_title} {film['year']}")) 
 
                 wait = WebDriverWait(driver, 10)
                 results = wait.until(
@@ -94,20 +111,20 @@ def parse_letterboxd():
                 driver.implicitly_wait(5)
                 done_films.append(film)
             except Exception as e:
-                # film['e'] = e
+                print(e)
                 skipped_films.append(film)
 
     driver.quit()
 
-    with open("./scripts/scrap/parsed_films.csv", "w", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=["title", "imageUrl", "directors", "synopsis", "rating"])
+    with open("./scripts/scrap/parsed_films2.csv", "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=["title", "imageUrl", "directors", "synopsis", "year", "rating"])
         writer.writeheader()
         writer.writerows(done_films)
 
-    with open("./scripts/scrap/skipped_films.csv", "w", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=["title", "imageUrl", "directors", "synopsis", "e"])
+    with open("./scripts/scrap/skipped_films2.csv", "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=["title", "imageUrl", "directors", "year", "synopsis",])
         writer.writeheader()
         writer.writerows(skipped_films)
     
-# get_metrograph_films(False)
+# get_metrograph_films(True)
 parse_letterboxd()
